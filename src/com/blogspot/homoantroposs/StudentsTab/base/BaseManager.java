7fab6cf3a5ca7f3e531bc852d59294c2eab7c61f
homoantropos <homoantropos@gmail.com>
2018-07-01 15:03:59 +0300

    second

--- src/com/blogspot/homoantroposs/StudentsTab/base/BaseManager.java
+++ src/com/blogspot/homoantroposs/StudentsTab/base/BaseManager.java
@@ -2,14 +2,45 @@ package com.blogspot.homoantroposs.StudentsTab.base;
 
 import com.blogspot.homoantroposs.StudentsTab.groups.GroupOfStudents;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class BaseManager {
 
-    GroupOfStudents group;
+    GroupOfStudents group1;
 
     public BaseManager() {}
 
     public void addGroup (GroupOfStudents group) {
+
         BaseOfGroups.getBase().add(group);
+        try {
+            FileOutputStream fos = new FileOutputStream("base.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(BaseOfGroups.getBase());
+            oos.close();
+        }catch (IOException e) {
+            System.out.println("Файл не знайдено.");
+        }
+    }
+
+    public void changeGroup (GroupOfStudents group) {
+        String nameOfGroup = group.getNameOfGroup();
+        for (GroupOfStudents gos : BaseOfGroups.getBase()) {
+            if (nameOfGroup.equals(gos.getNameOfGroup()))
+                group1 = gos;
+        }
+                BaseOfGroups.getBase().remove(group1);
+        BaseOfGroups.getBase().add(group);
+        try {
+            FileOutputStream fos = new FileOutputStream("base.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(BaseOfGroups.getBase());
+            oos.close();
+        }catch (IOException e) {
+            System.out.println("Файл не знайдено.");
+        }
     }
 
     public GroupOfStudents getGroupOfStudents (GroupOfStudents group) {
@@ -22,10 +53,10 @@ public class BaseManager {
         for (GroupOfStudents gof : BaseOfGroups.getBase()) {
             if (gof.getNameOfGroup().equals(nameOfGroup)) {
                 Integer index = BaseOfGroups.getBase().indexOf(gof);
-                group = BaseOfGroups.getBase().get(index);
+                group1 = BaseOfGroups.getBase().get(index);
             }
         }
-        return group;
+        return group1;
     }
 
 }
